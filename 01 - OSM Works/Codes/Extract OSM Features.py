11baeda9a14f3e18125824cf420d0e7f80b7642e
Allan Mogley <allanmogley@gmail.com>
2023-06-27 01:46:13 +0300

    Final Commit to organizing the folders

---
+++ 01 - OSM Works/Codes/Extract OSM Features.py
@@ -0,0 +1,14 @@
+import matplotlib
+import networkx as nx
+import osmnx as ox
+
+
+# download/model a street network for some city then visualize it
+G = ox.graph_from_place("Juja, Kiambu County, Kenya", network_type="drive")
+
+# convert your MultiDiGraph to an undirected MultiGraph
+M = nx.MultiGraph(G.to_undirected())
+# check if the graph is directed
+print(nx.is_directed(M))
+
+fig, ax = ox.plot_graph(M)
